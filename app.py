6e25aae681c73102a5733bb5ae4dee46b2515c5f
wharmond <43218226+wharmond@users.noreply.github.com>
2018-10-27 18:42:14 -0400

    Add files via upload
    These are from one of my CX assignments

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template, request,json
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return(render_template('hello.html'))
+
+@app.route("/signUp")
+def signUp():
+    return(render_template('signUp.html'))
+
+@app.route('/signUpUser', methods=['POST'])
+def signUpUser():
+    user =  request.form['username'];
+    password = request.form['password'];
+    p_err = []
+    if not (len(password)>7):
+        p_err.append(0)
+    if not (any(c.isdigit() for c in password)):
+        p_err.append(1)
+    if not (any(c.isupper() for c in password)):
+        p_err.append(2)
+    if p_err == []:
+        return(json.dumps({'status':'OK','user':user,'pass':password}))
+    else:
+        return(json.dumps({'status':'BAD','user':user,'pass':p_err}))
+if __name__ == "__main__":
+    app.run(debug= True)
